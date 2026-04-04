66de88cdff73aae86270c14b8e0088d7755fd55b
Amine Mustapha <mustaphaamine54@gmail.com>
2026-04-04 19:51:02 +0900

    create search tool, and modify get_files to return paths not file name

---
+++ LOGIC/tools.py
@@ -0,0 +1,30 @@
+import os
+from LOGIC.core import get_files
+
+
+def search_files(folder_path):
+
+    files = get_files(folder_path)
+
+    if not files:
+        return []
+
+    while True:
+        user = input("Please enter a file name to search or 'exit' to quit: ").strip()
+
+        if user.lower() == "exit":
+            return False
+
+        elif not user:
+            print("Please enter a valid file name or 'exit' to quit")
+            continue
+
+        result = []
+
+        for file in files:
+            filename = os.path.basename(file)
+
+            if user.lower() in filename.lower():
+                result.append(file)
+
+        return result
